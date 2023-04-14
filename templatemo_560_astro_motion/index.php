05b0040808e351b785a857af56a013510b267b36
EmilyOy <emilyoy92@gmail.com>
2023-04-14 11:04:30 +0800

    Update index.php

--- templatemo_560_astro_motion/index.php
+++ templatemo_560_astro_motion/index.php
@@ -478,11 +478,11 @@ https://templatemo.com/tm-560-astro-motion
                 <thead>
                   <tr>
                     <th class="product-thumbnail"><font color="white">Image</font></th>
-                    <th class="product-name">Product</th>
-                    <th class="product-price">Price</th>
-                    <th class="product-quantity">Quantity</th>
-                    <th class="product-total">Total</th>
-                    <th class="product-remove">Remove</th>
+                    <th class="product-name"><font color="white">Product</font></th>
+                    <th class="product-price"><font color="white">Price</font></th>
+                    <th class="product-quantity"><font color="white">Quantity</font></th>
+                    <th class="product-total"><font color="white">Total</font></th>
+                    <th class="product-remove"><font color="white">Remove</font></th>
                   </tr>
 
                 </thead>
