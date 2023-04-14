05576469121ac019c3fd3d595dfe282e5d3dba3a
EmilyOy <emilyoy92@gmail.com>
2023-04-14 14:54:18 +0800

    Update index.php

--- templatemo_560_astro_motion/index.php
+++ templatemo_560_astro_motion/index.php
@@ -711,23 +711,18 @@ https://templatemo.com/tm-560-astro-motion
           <td>
             <input type="text"></input>
           </td>
-         
+          <td><div class="shape-ex1">上傳</div>
+            <!-- <a href="cartdelete.php?goodsNum=<?php echo $row['goodsNum']?>"><font color="white"></font></a></button></td>
+   -->
     <?php 
       }
         ?>
             </tr>
-            <div class="shape-ex1">上傳</div>
-            <!-- <a href="cartdelete.php?goodsNum=<?php echo $row['goodsNum']?>"><font color="white"></font></a></button></td>
-   -->
       </form>
-
-                 
-
-                 
                 </tbody>
               </table>
             </div>
-
+rrr
             </div>
           </li>
           <li data-page-no="4">
@@ -809,32 +804,74 @@ https://templatemo.com/tm-560-astro-motion
               </div>
             </div>
           </li>
-
           <li data-page-no="5">
             <div class="mx-auto page-width-2">
               <div class="row">
-              <div class="col-md-6 me-0 ms-auto">
-                <h2 class="mb-6">LogIn登入</h2>
-                <h2 class="mb-6">登入即可更改資料</h2>
-              </div>
+                <div class="col-md-6 me-0 ms-auto">
+                  <h2 class="mb-4">登入</h2>
+                </div>
               </div>
               <div class="row">
                 <div class="col-md-6 tm-contact-left">
                   <form action="#" method="POST" class="contact-form">
                     <div class="input-group tm-mb-30">
-                      <input name="account" type="text" class="form-control rounded-0 border-top-0 border-end-0 border-start-0" placeholder="Account 帳號">
+                      <input
+                        name="帳號"
+                        type="text"
+                        class="form-control rounded-0 border-top-0 border-end-0 border-start-0"
+                        placeholder="Name"
+                      />
                     </div>
                     <div class="input-group tm-mb-30">
-                      <input name="password" type="text" class="form-control rounded-0 border-top-0 border-end-0 border-start-0" placeholder="Password 密碼">
+                      <input
+                        name="密碼"
+                        type="text"
+                        class="form-control rounded-0 border-top-0 border-end-0 border-start-0"
+                        placeholder="Email"
+                      />
                     </div>
-                    <div class="input-group justify-content-start">
-                      <input type="submit" class="btn btn-primary tm-btn-pad-2" value="Login">
+                    <div class="input-group justify-content-end">
+                      <input
+                        type="submit"
+                        class="btn btn-primary tm-btn-pad-2"
+                        value="登入"
+                      />
                     </div>
                   </form>
                 </div>
+                <div class="col-md-6 tm-contact-right">
+                  <p class="mb-4">
+                    有任何問題歡迎聯絡我們!
+                  </p>
+                  <div>
+                    Email:
+                    <a href="mailto:info@company.com" class="tm-link-white"
+                      >info@company.com</a
+                    >
+                  </div>
+                  <div class="tm-mb-45">
+                    Tel:
+                    <a href="tel:0100200340" class="tm-link-white"
+                      >010-020-0340</a
+                    >
+                  </div>
+                  <!-- Map -->
+                  <div class="map-outer">
+                    <div class="gmap-canvas">
+                      <iframe
+                        width="100%"
+                        height="400"
+                        id="gmap-canvas"
+                        src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                        frameborder="0"
+                        scrolling="no"
+                        marginheight="0"
+                        marginwidth="0"
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
               </div>
-
-              
             </div>
           </li>
           
