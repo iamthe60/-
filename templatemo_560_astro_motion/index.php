d800159fe590d90e70a525900cbb66bc1a47a938
Queena3237 <queena1935@gmail.com>
2023-04-14 16:23:07 +0800

    Update index.php

--- templatemo_560_astro_motion/index.php
+++ templatemo_560_astro_motion/index.php
@@ -74,13 +74,42 @@ https://templatemo.com/tm-560-astro-motion
                       <a class="nav-link" href="#0" data-no="4">聯絡我們</a>
                       <div class="circle"></div>
                     </li>
-                    <li>
+                    
+                    
+                    <?php
+                    if($_SESSION['account']!=""){
+                        ?>
+						
+                        <?php
+                        if($_SESSION['level']==admin){
+                            ?>
+                        
+                        <li class="nav-item"><a href="#"><span style="color:White;">上下架</span></a></li> 
+                        <li class="nav-item"><a href="logouttest.php"><span style="color:White;">Logout</span></a></li>
+                        <?php
+                        } 
+                        else{
+                        ?>
+                        <li class="nav-item">
+                        <a class="nav-link" href="logintest.php">登入啦幹</a>
+                        <div class="circle"></div>
+                        </li>
+                        <?php
+                        }
+                        ?>
+                    <?php 
+                    } 
+                    else{
+                        ?>
+                        
                     <li class="nav-item">
                       <a class="nav-link" href="logintest.php">登入啦幹</a>
                       <div class="circle"></div>
                     </li>
-
-                    </li>
+                    <?php
+                        }
+                        ?>
+                    
                   </ul>
                 </div>
               </nav>
@@ -802,81 +831,6 @@ https://templatemo.com/tm-560-astro-motion
               </div>
             </div>
           </li>
-<<<<<<< HEAD
-
-          
-=======
-          <li data-page-no="5">
-            <div class="mx-auto page-width-2">
-              <div class="row">
-                <div class="col-md-6 me-0 ms-auto">
-                  <h2 class="mb-4">登入</h2>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 tm-contact-left">
-                  <form action="#" method="POST" class="contact-form">
-                    <div class="input-group tm-mb-30">
-                      <input
-                        name="帳號"
-                        type="text"
-                        class="form-control rounded-0 border-top-0 border-end-0 border-start-0"
-                        placeholder="Name"
-                      />
-                    </div>
-                    <div class="input-group tm-mb-30">
-                      <input
-                        name="密碼"
-                        type="text"
-                        class="form-control rounded-0 border-top-0 border-end-0 border-start-0"
-                        placeholder="Email"
-                      />
-                    </div>
-                    <div class="input-group justify-content-end">
-                      <input
-                        type="submit"
-                        class="btn btn-primary tm-btn-pad-2"
-                        value="登入"
-                      />
-                    </div>
-                  </form>
-                </div>
-                <div class="col-md-6 tm-contact-right">
-                  <p class="mb-4">
-                    有任何問題歡迎聯絡我們!
-                  </p>
-                  <div>
-                    Email:
-                    <a href="mailto:info@company.com" class="tm-link-white"
-                      >info@company.com</a
-                    >
-                  </div>
-                  <div class="tm-mb-45">
-                    Tel:
-                    <a href="tel:0100200340" class="tm-link-white"
-                      >010-020-0340</a
-                    >
-                  </div>
-                  <!-- Map -->
-                  <div class="map-outer">
-                    <div class="gmap-canvas">
-                      <iframe
-                        width="100%"
-                        height="400"
-                        id="gmap-canvas"
-                        src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                        frameborder="0"
-                        scrolling="no"
-                        marginheight="0"
-                        marginwidth="0"
-                      ></iframe>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
->>>>>>> faaff916f6aaffe9d6f125c0532a52fa0c6bb183
           
           
         </ul>
