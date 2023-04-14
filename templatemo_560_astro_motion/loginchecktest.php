1046d222e0c0756f4735f48c5bf1bbf48055ef87
Queena3237 <queena1935@gmail.com>
2023-04-14 16:26:35 +0800

    Create loginchecktest.php

---
+++ templatemo_560_astro_motion/loginchecktest.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+ $account=$_POST['account'];
+ $password=$_POST['password'];
+ echo $account , $password;
+ $link = mysqli_connect('localhost','root','12345678','fjufreedge');
+ $sql = "select distinct * from login where account = '$account' and password = '$password'";
+ $result = mysqli_query($link, $sql);
+ if($row = mysqli_fetch_assoc($result))
+ {
+   $_SESSION['account'] = $row['account'];
+   $_SESSION['password'] = $row['password'];
+   $_SESSION['level'] = $row['level'];
+   header("location:index.php?method=message&message=登入成功");
+ }
+ else
+ {
+   header("location:index.php?method=message&message=登入失敗");
+ }
+?>
