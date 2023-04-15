95de872e9e99a90c4da3bf7c635362bf67aa584f
EmilyOy <emilyoy92@gmail.com>
2023-04-15 11:41:40 +0800

    Create message.php

---
+++ templatemo_560_astro_motion/message.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Generator" content="EditPlus®">
+        <meta name="Author" content="">
+        <meta name="Keywords" content="">
+        <meta name="Description" content="">
+        <title>Document</title>
+    </head>
+    <body>
+        <table width=400 align=center border=1 bordercolor=#FFBA3B cellspacing=0 cellpadding=0 ;>
+            <tr>
+                <td bgcolor=#FFBA3B >
+                提示：
+                </td>
+            </tr>
+            
+            <tr>
+                <td bgcolor=white align=center height=150>
+                <p align=center><font color=#FF0000 size=3><?php echo $_GET["message"]; ?></font></p>
+                </td>
+            </tr>
+           
+            <tr>
+                <td align=right bgcolor=#FFBA3B>
+                | <a href="index.php">回首頁</a> |
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
