43c164cf7d8b855d0a5de4cb96541ce2154f9acd
EmilyOy <emilyoy92@gmail.com>
2023-04-15 19:54:29 +0800

    Update foodaction.php

--- templatemo_560_astro_motion/foodaction.php
+++ templatemo_560_astro_motion/foodaction.php
@@ -1,7 +1,29 @@
 <?php
-$conn = new mysqli('localhost','root','','fjufreedge');
-if($conn->connect_error){
-    die('Error : ('. $conn->connect_error .')'.$conn->connect_error);
-}
+    if(!empty($_GET['foodaction'])){
+        $foodaction = $_GET['foodaction'];
+        $amount = $_GET['amount'];
+        $num = $_GET['num'];
+        if($foodaction =="上傳"){
+            $link = mysqli_connect('localhost','root','','fjufreedge');
+            $sql = "UPDATE `item` SET `amount`='$amount' WHERE `num`='1';";
+            $result = mysqli_query($link, $sql);
+            if(mysqli_query($link,$sql))
+            {
+                header("Location: message.php?message=上傳完成");
+               
+            }
+            else
+            {
+                header("Location: message.php?message=上傳失敗");
+                
+            }
+        }else{
+            $sql = "UPDATE `item` SET amount='0' where num = '$num';";
+        }
+    }
+
+
+
+
 
 ?>
