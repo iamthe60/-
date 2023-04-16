de8433e2f66d44cf92bb7c298530246854db221a
Queena3237 <queena1935@gmail.com>
2023-04-16 14:36:14 +0800

    Update index.php

--- templatemo_560_astro_motion/index.php
+++ templatemo_560_astro_motion/index.php
@@ -90,7 +90,7 @@ https://templatemo.com/tm-560-astro-motion
                         if($_SESSION['level']=="admin"){
                             ?>
                           <li class="nav-item">
-                          <a class="nav-link" href="#0" data-no="6" style="padding-left: 50px; padding-right: 50px">登出</a>
+                          <a class="nav-link" href="logouttest.php" data-no="6" style="padding-left: 50px; padding-right: 50px">登出</a>
                           <div class="circle"></div>
                           </li>
                         <?php
