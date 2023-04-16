868bf7321b257592bd62dbda84827235a0d5e077
EmilyOy <emilyoy92@gmail.com>
2023-04-16 15:16:32 +0800

    Update logouttest.php

--- templatemo_560_astro_motion/logouttest.php
+++ templatemo_560_astro_motion/logouttest.php
@@ -3,5 +3,5 @@ session_start();
 $_SESSION['account']="";
 $_SESSION['password']="";
 $_SESSION['level']="";
-header("location:index.php?method=message&message=登出成功");
+header("location:index.php?method=message&message=登出系統");
 ?>
