b56858a10ec92a27527565575639e2cfd3cf1295
aiyunnnnnnn <elise.chen92@gmail.com>
2023-04-25 23:19:25 +0800

    Update index.php

--- templatemo_560_astro_motion/index.php
+++ templatemo_560_astro_motion/index.php
@@ -24,13 +24,11 @@ https://templatemo.com/tm-560-astro-motion
 
 -->
 <?
-<<<<<<< Updated upstream
+
  
  $conn = new mysqli('localhost','root','12345678','fjufreedge');
-=======
  include "foodaction.php";
- $conn = new mysqli('localhost','root','e20030225','fjufreedge');
->>>>>>> Stashed changes
+
  if(isset($_POST['but_update'])){
     if(isset($_POST['update'])){
       foreach($_POST['update'] as $updatenum){
