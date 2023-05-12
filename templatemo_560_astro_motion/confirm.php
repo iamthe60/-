9bcd86679975ffe3f5b48e2dd977d9acf0d996bc
rayyych <raychen0609@gmail.com>
2023-05-13 04:04:41 +0800

    rr

--- templatemo_560_astro_motion/confirm.php
+++ templatemo_560_astro_motion/confirm.php
@@ -12,33 +12,187 @@ if (isset($_POST['selected_item'])) {
 
 $itemname = $_SESSION['selected_item'];
 ?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title>Confirmation Page</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <title>輔大食享冰箱</title>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/slick.css" type="text/css" />
+    <link rel="stylesheet" href="css/templatemo-style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+
+    <!--
+    
+TemplateMo 560 Astro Motion
+
+https://templatemo.com/tm-560-astro-motion
+
+-->
+
+
+
 </head>
+
 <body>
-	<h2>Confirmation Page</h2>
-	<p>You have selected the following items:</p>
-	<ul>
-		<li>Item Name: <?php echo $itemname1; ?></li>
-		<li>Item Name: <?php echo $itemname2; ?></li>
-		<li>Item Name: <?php echo $itemname3; ?></li>
-	</ul>
-	<p>If this information is correct, please click the "Confirm" button to proceed.</p>
-	<form action="confirm.php" method="post">
-    <?php 
-    $selected_items = $itemname1 . "," . $itemname2 . "," . $itemname3;
-    ?>
+    <video autoplay muted loop id="bg-video">
+        <source src="video/refri_video.mp4" type="video/mp4" />
+    </video>
+    <div class="page-container">
 
-<input type="hidden" name="selected_item" value="<?php echo $selected_items; ?>">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="cd-slider-nav">
+                        <nav class="navbar navbar-expand-lg" id="tm-nav">
+                            <a class="navbar-brand" href="index.php">輔大食享冰箱</a>
 
-		<input type="submit" name="confirm" value="Confirm">
-	</form>
-	<p>If you want to make changes, please click the "Back" button to return to the previous page.</p>
-	<form action="item.php" method="post">
-		<input type="submit" name="back" value="Back">
-	</form>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbar-supported-content" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbar-supported-content">
+                                <ul class="navbar-nav mb-2 mb-lg-0">
+
+                                    <?php
+                                    
+                                    
+                                    if ($_SESSION['level'] == "admin") {
+                                        ?>
+                                        <li class="nav-item">
+                                            <a class="nav-link" aria-current="page" href="index.php" data-no="1"
+                                                style="padding-left: 50px; padding-right: 50px">首頁</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <li class="nav-item  selected">
+                                            <a class="nav-link" href="item.php" data-no="2"
+                                                style="padding-left: 50px; padding-right: 50px">領取物資</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="upload.php" data-no="3"
+                                                style="padding-left: 50px; padding-right: 50px">上架/下架</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="volunteer.php" data-no="4"
+                                                style="padding-left: 50px; padding-right: 50px">加入我們</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="logouttest.php"
+                                                style="padding-left: 50px; padding-right: 50px">登出</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <li class="nav-item selected">
+                                            <a class="nav-link" aria-current="page" href="index.php" data-no="1"
+                                                style="padding-left: 85px; padding-right: 85px;padding-right: 85px">首頁</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="inventory.php" data-no="6"
+                                                style="padding-left: 85px; padding-right: 85px">庫存查詢</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="volunteer.php" data-no="4"
+                                                style="padding-left: 85px; padding-right: 85px">加入我們</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="login.php" data-no="5"
+                                                style="padding-left: 85px; padding-right: 85px">登入</a>
+                                            <div class="circle"></div>
+                                        </li>
+                                        <?php
+                                    }
+                                    ?>
+
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+
+            </div>
+            <div class="container-fluid tm-content-container">
+          <div class="position-relative page-width-1 "
+            style="margin-left:25% ; margin-top:auto; #cccccc solid;">
+            
+            <div class="container-fluid tm-content-container">
+            <div class="intro-left tm-bg-dark content-pad">
+
+              <div class="mx-auto page-width-2">
+                <div class="row">
+                  <div class="col-6.col-md-4">
+                  <p>You have selected the following items:</p>
+                                        <ul>
+                                            <li>Item Name: <?php echo $itemname1; ?></li>
+                                            <li>Item Name: <?php echo $itemname2; ?></li>
+                                            <li>Item Name: <?php echo $itemname3; ?></li>
+                                        </ul>
+                                        <p>If this information is correct, please click the "Confirm" button to proceed.</p>
+                                        <form action="confirm.php" method="post">
+                                        <?php 
+                                        $selected_items = $itemname1 . "," . $itemname2 . "," . $itemname3;
+                                        ?>
+
+                                        <input type="hidden" name="selected_item" value="<?php echo $selected_items; ?>">
+
+                                            <input type="submit" name="confirm" value="Confirm">
+                                        </form>
+                                        <p>If you want to make changes, please click the "Back" button to return to the previous page.</p>
+                                        <form action="item.php" method="post">
+                                            <input type="submit" name="back" value="Back">
+                                        </form>
+                           
+              </div>
+              
+            </div>
+          </div>
+          </div>
+            
+                                        
+           
+           
+                    
+                    <div class="container-fluid">
+                        <footer class="row mx-auto tm-footer">
+                            <div class="col-md-6 px-0">
+                                Copyright 2021 Astro Motion Company Limited. All rights reserved.
+                            </div>
+                            <div class="col-md-6 px-0 tm-footer-right">
+                                Designed by
+                                <a rel="sponsored" href="https://templatemo.com" target="_blank"
+                                    class="tm-link-white">TemplateMo</a>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+                <!-- Preloader, https://ihatetomatoes.net/create-custom-preloading-screen/ -->
+                <div id="loader-wrapper">
+                    <div id="loader"></div>
+                    <div class="loader-section section-left"></div>
+                    <div class="loader-section section-right"></div>
+                </div>
+                <script src="js/jquery-3.5.1.min.js"></script>
+                <script src="js/bootstrap.min.js"></script>
+                <script src="js/slick.js"></script>
+                <script src="js/templatemo-script.js"></script>
 </body>
+
 </html>
+
