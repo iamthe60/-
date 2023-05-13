c0040ad796c5a32f7cfcc05565940ea42620aa26
Queena3237 <queena1935@gmail.com>
2023-05-13 15:32:56 +0800

    Update login.php

--- templatemo_560_astro_motion/login.php
+++ templatemo_560_astro_motion/login.php
@@ -49,51 +49,60 @@ https://templatemo.com/tm-560-astro-motion
               <div class="collapse navbar-collapse" id="navbar-supported-content">
               <ul class="navbar-nav mb-2 mb-lg-0">
                  
-                 <?php
+              <?php
                   session_start();
                  if($_SESSION['level']=="admin"){
                      ?>
-                      <li class="nav-item">
-             <a class="nav-link" aria-current="page" href="index.php" data-no="1" style="padding-left: 50px; padding-right: 50px">首頁</a>
-             <div class="circle"></div>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="item.php" data-no="2" style="padding-left: 50px; padding-right: 50px">領取物資</a>
-             <div class="circle"></div>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="upload.php" data-no="3" style="padding-left: 50px; padding-right: 50px">上架/下架</a>
-             <div class="circle"></div>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="volunteer.php" data-no="4" style="padding-left: 50px; padding-right: 50px">加入我們</a>
-             <div class="circle"></div>
-           </li>
-                   <li class="nav-item selected">
-                   <a class="nav-link" href="logouttest.php" style="padding-left: 50px; padding-right: 50px">登出</a>
-                   <div class="circle"></div>
-                   </li>
-                 <?php
+                                    <li class="nav-item selected">
+                                        <a class="nav-link" aria-current="page" href="#0" data-no="1"
+                                            style="padding-left: 50px; padding-right: 50px">首頁</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="item.php" data-no="2"
+                                            style="padding-left: 50px; padding-right: 50px">領取物資</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="upload.php" data-no="3"
+                                            style="padding-left: 50px; padding-right: 50px">上架/下架</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="volunteerlogin.php" data-no="4"
+                                            style="padding-left: 50px; padding-right: 50px">志工專區</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="logouttest.php"
+                                            style="padding-left: 50px; padding-right: 50px">登出</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <?php
                  } 
                  else{
                  ?>
-                  <li class="nav-item selected">
-                    <a class="nav-link" aria-current="page" href="index.php" data-no="1" style="padding-left: 85px; padding-right: 85px;padding-right: 85px">首頁</a>
-                    <div class="circle"></div>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="inventory.php" data-no="6" style="padding-left: 85px; padding-right: 85px">庫存查詢</a>
-                    <div class="circle"></div>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="volunteer.php" data-no="4" style="padding-left: 85px; padding-right: 85px">加入我們</a>
-                    <div class="circle"></div>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="login.php" data-no="5" style="padding-left: 85px; padding-right: 85px">登入</a>
-                      <div class="circle"></div>
-                  </li>
-                 <?php
+                                    <li class="nav-item selected">
+                                        <a class="nav-link" aria-current="page" href="index.php" data-no="1"
+                                            style="padding-left: 85px; padding-right: 85px;padding-right: 85px">首頁</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="inventory.php" data-no="6"
+                                            style="padding-left: 85px; padding-right: 85px">庫存查詢</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="volunteerlogin.php" data-no="4"
+                                            style="padding-left: 85px; padding-right: 85px">志工專區</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="login.php" data-no="5"
+                                            style="padding-left: 85px; padding-right: 85px">登入</a>
+                                        <div class="circle"></div>
+                                    </li>
+                                    <?php
                  }
                  ?>
 
@@ -149,13 +158,6 @@ https://templatemo.com/tm-560-astro-motion
           </div>
     <div class="container-fluid">
       <footer class="row mx-auto tm-footer">
-        <div class="col-md-6 px-0">
-          Copyright 2021 Astro Motion Company Limited. All rights reserved.
-        </div>
-        <div class="col-md-6 px-0 tm-footer-right">
-          Designed by
-          <a rel="sponsored" href="https://templatemo.com" target="_blank" class="tm-link-white">TemplateMo</a>
-        </div>
       </footer>
     </div>
   </div>
