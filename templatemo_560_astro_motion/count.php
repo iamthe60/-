56a40b5ba840893f2050678cca7885651d1dc4be
EmilyOy <emilyoy92@gmail.com>
2023-05-16 14:34:02 +0800

    Update count.php

--- templatemo_560_astro_motion/count.php
+++ templatemo_560_astro_motion/count.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
-<<<<<<< HEAD
-
-$conn = new mysqli('localhost', 'root', '', 'fjufreedge');
-
-=======
 $conn = new mysqli('localhost', 'root','12345678', 'fjufreedge');
->>>>>>> 95b89aa44921c09ae664f4750c57e3dc98f46455
 $selected_items = $_POST['selected_item'];
 $items =explode(",", $selected_items);
 for($i = 0 ;$i <3;$i++){
@@ -18,35 +12,20 @@ for($i = 0 ;$i <3;$i++){
         $value = $row['amount'] - 1;
         $update_query = "UPDATE item SET amount = '$value' WHERE name = '$item';";
         mysqli_query($conn, $update_query);
-<<<<<<< HEAD
-=======
         header("location:index.php?method=message&message=刪減成功");
->>>>>>> 95b89aa44921c09ae664f4750c57e3dc98f46455
     } elseif ($row['area'] == '2') {
         $value = $row['amount'] - 1;
         $update_query = "UPDATE item SET amount = '$value' WHERE name = '$item';";
         mysqli_query($conn, $update_query);
-<<<<<<< HEAD
-=======
         header("location:index.php?method=message&message=刪減成功");
->>>>>>> 95b89aa44921c09ae664f4750c57e3dc98f46455
     } else {
         $value = $row['amount'] - 1;
         $update_query = "UPDATE item SET amount = '$value' WHERE name = '$item';";
         mysqli_query($conn, $update_query);
-<<<<<<< HEAD
-    }
-}
-
-mysqli_close($conn);
-header("Location: college.php");
-exit();
-=======
         header("location:index.php?method=message&message=刪減成功");
     }
 }
 
->>>>>>> 95b89aa44921c09ae664f4750c57e3dc98f46455
 ?>
 
 
