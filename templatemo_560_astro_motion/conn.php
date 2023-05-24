9010970c0caf1cda7fe2929644e4bcaa4f51e5c5
aiyunnnnnnn <elise.chen92@gmail.com>
2023-05-24 15:37:22 +0800

    abc

---
+++ templatemo_560_astro_motion/conn.php
@@ -0,0 +1,8 @@
+<?php
+ $host = "localhost";  
+ $db_user = "root";  
+ $db_pass = "e20030225";  
+ $dbname = "tut";  
+
+ $connect = new PDO("mysql:host=$host; dbname=$dbname", $db_user, $db_pass); 
+?>
