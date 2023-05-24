212aebe9c280d4b65054597d25c6f25e148decb8
EmilyOy <emilyoy92@gmail.com>
2023-05-24 15:37:53 +0800

    Update volunteer.php

--- templatemo_560_astro_motion/volunteer.php
+++ templatemo_560_astro_motion/volunteer.php
@@ -163,48 +163,8 @@ https://templatemo.com/tm-560-astro-motion
             </div>
             <div class="row">
                 <div class="col-md-12">
-                <div class="container py-5" id="page-container">
-        <div class="row">
-            <div class="col-md-9"  style="background-color:#E8E8E8">
-                <div id="calendar"></div>
-            </div>
-            <div class="col-md-3" style="background-color:#E8E8E8; padding-top:50px" >
-                <div class="cardt rounded-0 shadow">
-                    <div class="card-header bg-gradient bg-primary text-light">
-                        <h5 class="card-title">Schedule Form</h5>
-                    </div>
-                    <div class="card-body">
-                        <div class="container-fluid">
-                            <form action="save_schedule.php" method="post" id="schedule-form">
-                                <input type="hidden" name="id" value="">
-                                <div class="form-group mb-2">
-                                    <label for="title" class="control-label">Title</label>
-                                    <input type="text" class="form-control form-control-sm rounded-0" name="title" id="title" required>
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="description" class="control-label">Description</label>
-                                    <textarea rows="3" class="form-control form-control-sm rounded-0" name="description" id="description" required></textarea>
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="start_datetime" class="control-label">Start</label>
-                                    <input type="datetime-local" class="form-control form-control-sm rounded-0" name="start_datetime" id="start_datetime" required>
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="end_datetime" class="control-label">End</label>
-                                    <input type="datetime-local" class="form-control form-control-sm rounded-0" name="end_datetime" id="end_datetime" required>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <div class="text-center">
-                            <button class="btn btn-primary btn-sm rounded-0" type="submit" form="schedule-form"><i class="fa fa-save"></i> Save</button>
-                            <button class="btn btn-default border btn-sm rounded-0" type="reset" form="schedule-form"><i class="fa fa-reset"></i> Cancel</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+              
+      
     </div>
     <!-- Event Details Modal -->
     <div class="modal fade" tabindex="-1" data-bs-backdrop="static" id="event-details-modal">
