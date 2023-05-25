edecaafcd304b975b82fcd53775d72eba0a46a74
aiyunnnnnnn <elise.chen92@gmail.com>
2023-05-25 19:53:16 +0800

    a

--- templatemo_560_astro_motion/index2.php
+++ templatemo_560_astro_motion/index2.php
@@ -2,16 +2,14 @@
 <html>
 
 <head>
-    <title>jQuery Event Booking Using Fullcalendar With PHP</title>
     <link rel="stylesheet" href="css/fullcalendar.css" />
     <link rel="stylesheet" href="css/style2.css" />
 
 </head>
 
 <body>
-
     <div class="booking">
-        <div id="event_calendar" style=weight: 50px;></div>
+        <div id="event_calendar"></div>
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- templatemo_560_astro_motion/insert.php
+++ templatemo_560_astro_motion/insert.php
@@ -1,21 +1,13 @@
 <?php
 include ('conn.php');
-$conn = new mysqli('localhost','root','','fjufreedge');
-$id = $_SESSION['id'];
-$name = $_SESSION['name'];
-$count = "SELECT MAX(num) AS total FROM `events`;";
-$result = mysqli_query($conn, $count);
-$row=mysqli_fetch_assoc($result);
-$num = $row['total']+1 ;
 
 if (isset($_POST["title"]))
 {
     $query = "
-    INSERT INTO `events`(`num`, `id`, `title`, `start_event`, `end_event`)
-    VALUES ('$num','$id',:title,:start_event, :end_event)
- "
- 
- ;
+ INSERT INTO events 
+ (title, start_event, end_event) 
+ VALUES (:title, :start_event, :end_event)
+ ";
     $statement = $connect->prepare($query);
     $statement->execute(array(
         ':title' => $_POST['title'],
