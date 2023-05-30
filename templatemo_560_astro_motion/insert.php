b8b65ed09776c064968e407419a96fcd92ae22df
aiyunnnnnnn <elise.chen92@gmail.com>
2023-05-30 16:41:19 +0800

    Update insert.php

--- templatemo_560_astro_motion/insert.php
+++ templatemo_560_astro_motion/insert.php
@@ -7,17 +7,32 @@ if (isset($_POST["title"]) && isset($_POST["start"]) && isset($_POST["end"])) {
     $start = $_POST["start"];
     $end = $_POST["end"];
 
-    $selectQuery = "SELECT COUNT(*) AS COUNT FROM events WHERE title = :title AND start_event = :start";
+    $countQuery = "SELECT COUNT(start_event) AS COUNT FROM events WHERE start_event = :start";
     $insertQuery = "INSERT INTO events (title, start_event, end_event) VALUES (:title, :start_event, :end_event)";
 
-    $selectStatement = $connect->prepare($selectQuery);
-    $selectStatement->bindParam(':title', $title);
-    $selectStatement->bindParam(':start', $start);
-    $selectStatement->execute();
-    $countResult = $selectStatement->fetch(PDO::FETCH_ASSOC);
+    $statement = $connect->prepare($countQuery);
+    $statement->bindParam(':start', $start);
+    $statement->execute();
+    $countResult = $statement->fetch(PDO::FETCH_ASSOC);
     $count = $countResult['COUNT'];
+    
+    $id = $_POST["id"];
 
-    if ($count == 0) {
+   $existingQuery = "SELECT COUNT(*) AS COUNT FROM events WHERE id != :id AND (start_event BETWEEN :start AND :end OR end_event BETWEEN :start AND :end)";
+       $existingStatement = $connect->prepare($existingQuery);
+$existingStatement->bindParam(':id', $id);
+$existingStatement->bindParam(':start', $start);
+$existingStatement->bindParam(':end', $end);
+$existingStatement->execute();
+$existingResult = $existingStatement->fetch(PDO::FETCH_ASSOC);
+$existingCount = $existingResult['COUNT'];
+
+if ($existingCount > 0) {
+    echo "該時間段已排班";
+    exit;
+}
+
+    if ($count < 2) {
         $insertStatement = $connect->prepare($insertQuery);
         $insertStatement->bindParam(':title', $title);
         $insertStatement->bindParam(':start_event', $start);
@@ -29,6 +44,9 @@ if (isset($_POST["title"]) && isset($_POST["start"]) && isset($_POST["end"])) {
         } else {
             echo "error";
         }
-    } 
-} 
+    } else {
+        echo "已額滿";
+    }
+
+}
 ?>
